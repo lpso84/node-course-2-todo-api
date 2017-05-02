85fcf9c477ab9d50303f82396ac96dc78cdd6f96
“luissoliveira” <luissoliveira@deloitte.pt>
2017-05-02 17:30:15 +0100

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ var app = express();
 
 app.use(bodyParser.json());
 
-
 app.post('/todos', (req, res) =>{
   var todo = new Todo({
     text: req.body.text
@@ -26,3 +25,6 @@ app.post('/todos', (req, res) =>{
 app.listen(3000, () =>{
   console.log('Started on port 3000');
 });
+
+
+module.exports = {app};
