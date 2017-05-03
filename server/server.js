6a0c77b4426e2e98266c6cf35f30b1d4e40770c1
“luissoliveira” <luissoliveira@deloitte.pt>
2017-05-03 17:35:16 +0100

    nova tentativa

--- server/server.js
+++ server/server.js
@@ -1,22 +1,13 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-const {
-  ObjectID
-} = require('mongodb');
-
-var {
-  mongoose
-} = require('./db/mongoose.js')
-var {
-  Todo
-} = require('./models/todo.js');
-var {
-  Users
-} = require('./models/user.js');
-
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -29,14 +20,12 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({
-      todos
-    });
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
@@ -54,18 +43,14 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.send({
-      todo
-    });
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
 
-module.exports = {
-  app
-};
+module.exports = {app};
