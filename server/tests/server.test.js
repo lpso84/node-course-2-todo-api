bad62e0eedc4b7fb24af20c3f4885f8934ad9d32
“luissoliveira” <luissoliveira@deloitte.pt>
2017-05-03 14:10:05 +0100

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {
   app
@@ -9,8 +10,10 @@ const {
 } = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -80,4 +83,32 @@ describe('GET /todos', () => {
       })
       .end(done);
   })
-})
+});
+
+describe('Get /todos/:id', () =>{
+  it('should return todo doc', (done) =>{
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) =>{
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found' ,(done) =>{
+    var hexID = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/${hexID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 for non-obj ids', (done) =>{
+    request(app)
+      .get(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+});
